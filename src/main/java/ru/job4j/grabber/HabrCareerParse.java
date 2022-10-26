4288572fed9d28fcd1ec59a53236c73d1fa05668
java73core <mrvandrey@gmail.com>
2022-10-27 02:33:11 +0400

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,26 +1,37 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HabrCareerParse {
 
-    private static  final int NUM_PAGE = 5;
+    private static  final int NUM_PAGE = 1;
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    private static String retrieveDescription(String link) throws IOException {
+        List<String> contentVacancy = new ArrayList<>();
+        Document document = Jsoup.connect(link).get();
+        Elements rows = document.select(".collapsible-description");
+        rows.forEach(row -> {
+                    Element contentElement = row.select(".style-ugc").first();
+                    contentVacancy.add(contentElement.text());
+                });
+        return contentVacancy.stream().collect(Collectors.joining());
+    }
+
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= NUM_PAGE; i++) {
             String pageNumber = String.format("%s?page=%s", PAGE_LINK, i);
-            Connection connection = Jsoup.connect(pageNumber);
-            Document document = connection.get();
+            Document document = Jsoup.connect(pageNumber).get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
                 Element titleElement = row.select(".vacancy-card__title").first();
@@ -30,7 +41,11 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 String time = String.format("%s", dateElement.attr("datetime"));
-                System.out.printf("%s %s %s%n", vacancyName, link, time);
+                try {
+                    System.out.printf("%s %s %s%n %s%n", vacancyName, link, time, retrieveDescription(link));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             });
         }
     }
